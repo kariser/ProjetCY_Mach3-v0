4ceb3232d58c30ccff7a66f212626d432134445a
kbm-dev <a.karim@kbm.ma>
2023-05-26 17:11:16 +0100

    version-0.0

---
+++ main.c
@@ -0,0 +1,507 @@
+//grid verifying pour qu'il n y ait pas 3 matching cells au départ
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#define maxGridSize 26
+#define EMPTY_CELL ' '
+
+// Structure représentant une cellule du plateau
+typedef struct {
+    char symbol;
+    int color;
+    int validMatches;
+} Cell;
+
+char  letters[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', };
+ 
+ char symbols[] = { 'A', 'B', 'C', 'D', 'E', 'F' };
+ 
+int pNumberOfSymbols =5;
+
+int pGridLines =8;
+
+int pGridColumns =8;
+
+Cell board[maxGridSize][maxGridSize];
+
+
+int score =0;
+int validsSwaps =0;
+
+
+// Couleurs des lettres
+#define COLOR_RED    "\x1b[31m"
+#define COLOR_GREEN  "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE   "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN   "\x1b[36m"
+#define COLOR_RESET  "\x1b[0m"
+
+
+
+void fillBoard() 
+{
+   
+    for (int i = 0; i < pGridLines; i++) {
+        for (int j = 0; j < pGridColumns; j++) 
+        {
+          
+            int index = rand() % pNumberOfSymbols;
+
+                board[i][j].symbol = symbols[index];
+                board[i][j].color = index;
+                board[i][j].validMatches = 0;
+
+            // verifier à gauche
+            if ((j>=2 &&    board[i][j-1].symbol==symbols[index] &&    board[i][j-2].symbol==symbols[index]  )
+               ||
+               (j==pGridColumns-1 &&    board[i][j-1].symbol==symbols[index] &&    board[i][0].symbol==symbols[index]  )
+               ||
+               (j==pGridColumns-1 &&    board[i][1].symbol==symbols[index] &&    board[i][0].symbol==symbols[index]  )
+             
+
+                 ) 
+            {
+                    char  varBadLeftSymbol= symbols[index];
+
+                    while (varBadLeftSymbol == symbols[index])
+                     {
+                            index = rand() % pNumberOfSymbols;
+                           
+                      }
+                
+                 board[i][j].symbol = symbols[index];
+                 board[i][j].color = index;
+                 board[i][j].validMatches = 0;
+
+            }
+            // verifier en dessus
+            if ( (i>=2 &&    board[i-1][j].symbol==symbols[index] &&    board[i-2][j].symbol==symbols[index]  )
+
+            ||
+               (i==pGridLines-1 &&    board[i-1][j].symbol==symbols[index] &&    board[0][j].symbol==symbols[index]  )
+            ||
+               (i==pGridLines-1 &&    board[1][j].symbol==symbols[index] &&    board[0][j].symbol==symbols[index]  )
+              )
+            {
+                    char  varBadUpSymbol= symbols[index];
+
+                    while (varBadUpSymbol == symbols[index] || varBadUpSymbol == symbols[index] )
+                     {
+                            index = rand() % pNumberOfSymbols;
+                           
+                      }
+                
+                 board[i][j].symbol = symbols[index];
+                 board[i][j].color = index;
+                 board[i][j].validMatches = 0;
+
+            }
+
+
+
+        }
+    }
+}
+
+
+int MvtH(int varIndex, int varNumber)
+{
+
+   int result =  (varIndex + varNumber )% pGridColumns;
+   if (result<0) result=pGridColumns+result;
+   return result;
+
+
+}
+
+
+int MvtV(int varIndex, int varNumber)
+{
+
+   int result =  (varIndex + varNumber )% pGridLines;
+   if (result<0) result=pGridLines+result;
+   return result;
+
+
+}
+
+
+
+
+
+// Fonction d'affichage du plateau
+void printBoard()
+ {
+    printf("   ");
+    for (int i = 0; i < pGridColumns; i++)
+        printf("%c ", letters[i]);
+    printf("\n");
+
+    for (int i = 0; i < pGridLines; i++) {
+        printf("%2d ", i + 1);
+        for (int j = 0; j < pGridColumns; j++) {
+            int color = board[i][j].color;
+            const char* colorCode;
+            switch (color) {
+                case 0:
+                    colorCode = COLOR_MAGENTA;
+                    break;
+                case 1:
+                    colorCode = COLOR_RED;
+                    break;
+                case 2:
+                    colorCode = COLOR_GREEN;
+                    break;
+                case 3:
+                    colorCode = COLOR_YELLOW;
+                    break;
+                case 4:
+                    colorCode = COLOR_BLUE;
+                    break;
+                case 5:
+                    colorCode = COLOR_MAGENTA;
+                    break;
+                
+                    
+                default:
+                    colorCode = COLOR_RESET;
+                    break;
+            }
+            printf("%s%c%s ", colorCode, board[i][j].symbol, COLOR_RESET);
+        }
+        printf("\n");
+    }
+}
+
+void printBoardWithMaches()
+{
+    
+    printf("   ");
+    for (int i = 0; i < pGridLines; i++)
+        printf("%d ", i + 1);
+    printf("\n");
+
+    for (int i = 0; i < pGridLines; i++) {
+        printf("%2d ", i + 1);
+        for (int j = 0; j < pGridColumns; j++) {
+            int color = board[i][j].color;
+            const char* colorCode;
+            switch (color) {
+                case 0:
+                    colorCode = COLOR_MAGENTA;
+                    break;
+                case 1:
+                    colorCode = COLOR_RED;
+                    break;
+                case 2:
+                    colorCode = COLOR_GREEN;
+                    break;
+                case 3:
+                    colorCode = COLOR_YELLOW;
+                    break;
+                case 4:
+                    colorCode = COLOR_BLUE;
+                    break;
+                case 5:
+                    colorCode = COLOR_MAGENTA;
+                    break;
+                
+                    
+                default:
+                    colorCode = COLOR_RESET;
+                    break;
+            }
+              
+            printf("%s%d%s ", colorCode, board[i][j].validMatches  , COLOR_RESET);
+        }
+        printf("\n");
+    }
+}
+
+   
+
+
+
+
+ 
+//gravity
+
+ void applyGravity(int i, int j )
+
+  {
+
+                board[i][j].symbol= EMPTY_CELL;
+                board[i][j].color= 10;
+                board[i][j].validMatches= 0;
+
+                score++;
+
+       if (i== 0)
+        {
+              // printf("Cell-0 i %d j %d \n",i,j);
+                board[i][j].symbol= EMPTY_CELL;
+                board[i][j].color= 10;
+                board[i][j].validMatches= 0;
+        }
+        else 
+            {    
+                   for (int k = i; k >0 ; k--)  
+                    { 
+                        
+                         if (board[k-1][j].symbol!=EMPTY_CELL)  
+                         {
+                             
+                              board[k][j] = board[k-1][j] ; 
+                         
+                               board[k-1][j].symbol= EMPTY_CELL;
+                               board[k-1][j].color= 10;
+                               board[k-1][j].validMatches= 0;
+                         
+                         }
+
+                     
+                        //  if( j==7) printf("Cell i %d j %d - Mvts  k-1 %d j %d vers k %d j %d  \n",i,j,k-1,j,k,j);
+                    }
+                   
+                    board[0][j].symbol= EMPTY_CELL;
+                    board[0][j].color= 10;
+                    board[0][j].validMatches= 0;
+
+            }
+
+   
+
+  }
+
+
+
+int  ExistingSymbol( char symbol, int MinNumber) {
+
+    int count = 0;
+     for (int i = 0; i < pGridLines; i++) 
+            {       
+                for (int j = 0; j < pGridColumns; j++)
+                    {
+                        if (board[i][j].symbol == symbol) count++;
+                        if (count==MinNumber) return 1;
+                    }
+            }
+    return 0;
+}
+  
+ 
+
+// Vérifie si le plateau contient un groupe d'au moins trois éléments identiques
+int hasMatchingCells() {
+    // Vérification des lignes
+    for (int i = 0; i < pGridLines; i++) {
+        int count = 1;
+        for (int j = 0; j < pGridColumns; j++) {
+            if (board[i][j].symbol != EMPTY_CELL && board[i][j].symbol == board[i][MvtH(j,+1)].symbol)
+               count++;
+            else
+                count = 1;
+
+        if ((count >= 2) && ExistingSymbol(board[i][j].symbol,3)==1)
+                return 1;
+        }
+    }
+
+    // Vérification des colonnes
+    for (int j = 0; j < pGridColumns; j++) {
+        int count = 1;
+        for (int i = 0; i < pGridLines; i++) {
+            if (board[i][j].symbol != EMPTY_CELL && board[i][j].symbol == board[MvtV(i, +1)][j].symbol)
+                count++;
+            else
+                count = 1;
+
+            if ((count >= 2) && ExistingSymbol(board[i][j].symbol,3)==1)
+                return 1;
+        }
+    }
+
+    return 0;
+}
+ 
+// Remplit le plateau avec des symboles aléatoires
+
+
+
+// Vérifie si un mouvement est valide
+int isValidMove(int row1, int col1, int row2, int col2) {
+     
+    char varSymbol= board[row1][col1].symbol;
+
+    if (
+
+         (  board[row2][MvtH(col2,-1)].symbol==varSymbol  && board[row2][MvtH(col2,+1)].symbol==varSymbol )
+          ||
+        (  board[row2][MvtH(col2,+1)].symbol==varSymbol  && board[row2][MvtH(col2,+2)].symbol==varSymbol )
+          ||
+        (  board[row2][MvtH(col2,-1)].symbol==varSymbol  && board[row2][MvtH(col2,-2)].symbol==varSymbol )
+
+          ||
+
+         (  board[MvtV(row2,-1)][col2].symbol==varSymbol  && board[MvtV(row2,+1)][col2].symbol==varSymbol )
+          ||
+        (  board[MvtV(row2,+1)][col2].symbol==varSymbol  && board[MvtV(row2,+2)][col2].symbol==varSymbol )
+          ||
+        (  board[MvtV(row2,-1)][col2].symbol==varSymbol  && board[MvtV(row2,-2)][col2].symbol==varSymbol )
+
+       )
+        return 1;
+    else
+        return 0;
+}
+
+
+int isMaching(int row1, int col1)  {
+   
+    // Horizontal
+    char varSymbol= board[row1][col1].symbol;
+
+     if (varSymbol==EMPTY_CELL) return 0;
+
+    if (
+         (  board[row1][MvtH(col1,-1)].symbol==varSymbol  && board[row1][MvtH(col1,+1)].symbol==varSymbol )
+          ||
+        (  board[row1][MvtH(col1,+1)].symbol==varSymbol  && board[row1][MvtH(col1,+2)].symbol==varSymbol )
+          ||
+        (  board[row1][MvtH(col1,-1)].symbol==varSymbol  && board[row1][MvtH(col1,-2)].symbol==varSymbol )
+
+            ||
+
+         (  board[MvtV(row1,-1)][col1].symbol==varSymbol  && board[MvtV(row1,+1)][col1].symbol==varSymbol )
+          ||
+        (  board[MvtV(row1,+1)][col1].symbol==varSymbol  && board[MvtV(row1,+2)][col1].symbol==varSymbol )
+          ||
+        (  board[MvtV(row1,-1)][col1].symbol==varSymbol  && board[MvtV(row1,-2)][col1].symbol==varSymbol )
+
+
+       )
+        return 1;
+    else
+        return 0;
+}
+// Effectue un mouvement en échangeant les cellules sélectionnées
+void makeMove( int row1, int col1, int row2, int col2) {
+
+ Cell temp = board[row1][col1];
+ board[row1][col1]=board[row2][col2];
+ board[row2][col2]=temp;
+
+ validsSwaps++;
+
+}
+
+// Supprime les groupes d'éléments identiques du plateau
+int markMatches() {
+    int matchesFound = 0;
+
+        // Marquer les celllules à supprimer
+        for (int i = 0; i < pGridLines; i++) 
+        {
+            
+            for (int j = 0; j < pGridColumns; j++) 
+            {
+
+                if (isMaching(i,j))  
+                  { board[i][j].validMatches=1;
+                     matchesFound=1;
+                  }
+     
+            }
+        }
+  return matchesFound;
+   
+}
+
+void removeMatches() 
+ {
+  
+
+        // Marquer les celllules à supprimer
+        for (int i = 0; i < pGridLines; i++) 
+        {
+            
+            for (int j = 0; j < pGridColumns; j++) 
+            {
+
+             
+                if ( board[i][j].validMatches)     applyGravity(i,j); 
+                                                      
+            }
+        }
+
+ 
+
+    }
+ 
+
+int main() {
+    
+    int row1, col1, row2, col2;
+    
+   
+      
+         printf("Indiquer Nombre de lignes et colonnes de la matrice : ");
+        scanf("%d %d", &pGridLines, &pGridColumns);
+
+        printf("Indiquer le nombre de symboles (4 à 6) : ");
+        scanf("%d", &pNumberOfSymbols);
+
+        fillBoard();
+    
+         printBoard();
+         
+
+    while (hasMatchingCells()) 
+     { 
+        
+        printf("Select a cell to swap (ligne colonne) : ");
+        scanf("%d %d", &row1, &col1);
+        
+         col1 =col1-'A';
+         printf("Select %d %d", &row1, &col1);  
+
+        printf("Sélectionnez une autre cellule à changer (ligne colonne) : ");
+        scanf("%d %d", &row2, &col2);
+
+        col2 =col2-'A';
+         printf("Select %d %d", &row2, &col2);  
+
+
+
+        if (isValidMove(row1 - 1, col1 - 1, row2 - 1, col2 - 1))
+         {                
+              makeMove( row1 - 1, col1 - 1, row2 - 1, col2 - 1);
+               
+              printf("Matrice avec mouvement et avant suppression\n");
+              printBoard();
+                
+               while   (markMatches())  {   removeMatches();  }
+
+              printf("Matrice apres suppresion\n");                     
+              printBoard();   
+
+               printf("Score : %d en %d Mouvements\n", score,validsSwaps);                           
+
+         }
+        else {
+            printf("Mouvement invalide. Veuillez reessayer.\n");
+             }
+   
+    }
+    printf("ExistMatchingCells  : %d\n",hasMatchingCells());
+
+    printf("Jeu terminé. Aucun mouvement possible.\n");
+
+    return 0;
+}
+ 
